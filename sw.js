e177449ab82402881c2692fc71738e4cd35b324c
cristian-jfv <151955146+cristian-jfv@users.noreply.github.com>
2024-10-03 06:10:59 +0000

    Deploying to gh-pages from @ cristian-jfv/decay-radiation-search@e1fd3756a57aa7a15d1c75207ef44e920ade705a 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'decay-radiation-search';
+var filesToCache = [
+  './',
+  './index.html',
+  './decay-radiation-search.js',
+  './decay-radiation-search.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
